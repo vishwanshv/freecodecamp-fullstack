8c83a531317f5467e92581adc62a27cbaab66243
Vishwansh <vishwanshv@gmail.com>
2026-09-11 10:42:29 +0530

    Add Trivia Bot 1st Lab Exercise

---
+++ javascript-fcc/labs/trivia-bot/script.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName ="rudy";
+let botLocation ="Hog Mountain";
+let favoriteLanguage ="JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " +  favoriteLanguage + ".");
+
+let codingFact= favoriteLanguage + " was created in just 10 days!  "
+
+console.log(codingFact);
+
+codingFact="Despite the similar names, " + favoriteLanguage + "and Java are completely different languages.";
+
+console.log(codingFact);
+
+codingFact= favoriteLanguage + " runs in every major web browser without installation."
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - rudy from Hog Mountain.");
