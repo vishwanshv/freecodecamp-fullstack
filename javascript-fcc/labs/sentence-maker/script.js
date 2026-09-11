fb7a03b93c2c09e63dc2e5b662bc938ef56bc8e6
Vishwansh <vishwanshv@gmail.com>
2026-09-11 10:47:43 +0530

    Add Sentence Maker 2nd Lab Exercise

---
+++ javascript-fcc/labs/sentence-maker/script.js
@@ -0,0 +1,22 @@
+let adjective = "brave";
+let noun = "dragon";
+let verb = "sneezing";
+let place = "mountain";
+let adjective2 = "huge";
+let noun2 = "cookies";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory);
+
+// Reassign values for the second story
+adjective = "mysterious";
+noun = "wizard";
+verb = "dancing";
+place = "castle";
+adjective2 = "tiny";
+noun2 = "pancakes";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
